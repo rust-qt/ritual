1cfa6270a870be648124cf4cd191aa8afe715136
Ri <ri@idzaaus.org>
2016-10-09 22:50:52 +0300

    old files removed

--- src/doc_parser_support/cpp_data.rs
+++
@@ -1,9 +0,0 @@
-use doc_parser_support::cpp_header_data::CppHeaderData;
-use doc_parser_support::cpp_type_map::CppTypeMap;
-
-#[derive(Debug, Clone)]
-pub struct DocCppData {
-  pub headers: Vec<CppHeaderData>,
-  pub types: CppTypeMap,
-  pub classes_blacklist: Vec<String>,
-}
--- src/doc_parser_support/mod.rs
+++
@@ -1,9 +0,0 @@
-
-pub mod cpp_data;
-pub mod cpp_header_data;
-pub mod cpp_type_map;
-pub mod extractor_actions_generator;
-pub mod parsers_consistency_checker;
-pub mod read_extracted_info;
-pub mod read_parse_result;
-pub mod enums;
