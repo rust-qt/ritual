7a238ed4a7610509be8b440f616dfbc74637a6bb
Pavel Strakhov <ri@idzaaus.org>
2017-04-18 20:32:12 +0300

    fix test

--- cpp_to_rust/cpp_to_rust_generator/src/tests/cpp_parser.rs
+++ cpp_to_rust/cpp_to_rust_generator/src/tests/cpp_parser.rs
@@ -27,7 +27,7 @@ fn run_parser(code: &'static str) -> CppData {
                                      tmp_cpp_path: dir.path().with_added("1.cpp"),
                                      name_blacklist: Vec::new(),
                                      framework_paths: Vec::new(),
-                                     flags: Vec::new(),
+                                     clang_arguments: Vec::new(),
                                    },
                                    Vec::new())
       .unwrap();
