f1b22bad4b140d693b91da2a099fc37570b0e617
Pavel Strakhov <ri@idzaaus.org>
2019-01-13 03:30:17 +0300

    fix vagrant script path

--- vagrant/linux/run_qt_ritual_moqt.sh
+++ vagrant/linux/run_qt_ritual_moqt.sh
@@ -7,6 +7,6 @@ $dir/moqt.sh
 export MOQT_PATH=$HOME/moqt
 
 export CPLUS_INCLUDE_PATH=$MOQT_PATH/moqt_core/include
-export LIBRARY_PATH=$MOQT_PATH/moqt/moqt_core/lib
+export LIBRARY_PATH=$MOQT_PATH/moqt_core/lib
 
 $dir/run_qt_ritual.sh -w $HOME/moqt_workspace "$@"
