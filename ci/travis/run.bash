296bbc59fc120ca6dcf68cccb5aea54d17ec8b7c
Pavel Strakhov <ri@idzaaus.org>
2019-02-03 20:32:37 +0300

    fix ci scripts

--- ci/travis/run.bash
+++ ci/travis/run.bash
@@ -6,10 +6,9 @@ export RUST_BACKTRACE=1
 export CPP_TO_RUST_TEMP_TEST_DIR=$HOME/cpp_to_rust_temp_test_dir
 mkdir -p "$CPP_TO_RUST_TEMP_TEST_DIR"
 
+source "$TRAVIS_BUILD_DIR/ci/travis/setup_clang.bash"
+
 cd "$TRAVIS_BUILD_DIR"
-
-source ci/travis/setup_clang.bash
-
 rustup component add clippy
 rustup component add rustfmt
 cargo clippy --all-targets
