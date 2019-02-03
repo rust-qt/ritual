7e084ca90c8761ca3e114a06d30fba54e7bf492e
Pavel Strakhov <ri@idzaaus.org>
2019-02-03 19:27:19 +0300

    move moqt to qt_ritual/test_assets

--- vagrant/linux/moqt.sh
+++ vagrant/linux/moqt.sh
@@ -5,7 +5,7 @@ set -e
 cd ~
 mkdir -p build_moqt/build_moqt_core
 cd build_moqt/build_moqt_core
-cmake -DCMAKE_INSTALL_PREFIX=$HOME/moqt/moqt_core /vagrant/moqt/moqt_core
+cmake -DCMAKE_INSTALL_PREFIX=$HOME/moqt/moqt_core /vagrant/qt_ritual/test_assets/moqt/moqt_core
 make install
 
 
