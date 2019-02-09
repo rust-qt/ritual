e219d2de6fc16e451879c41c5c3f8dcc3f72c347
Pavel Strakhov <ri@idzaaus.org>
2019-02-10 00:25:29 +0300

    clean up basic_class test

--- qt_ritual/crate_templates/moqt_core/tests/basic_class.rs
+++ qt_ritual/crate_templates/moqt_core/tests/basic_class.rs
@@ -1,14 +1,15 @@
 use moqt_core::BasicClass;
 
 #[test]
-fn class1_constructor1() {
+fn basic_class() {
     unsafe {
         let mut v = BasicClass::new(1);
         assert_eq!(v.foo(), 1);
         v.set_foo(5);
         assert_eq!(v.foo(), 5);
-        assert_eq!(v.public_int_field(), 1);
-        v.set_public_int_field(3);
-        assert_eq!(v.public_int_field(), 3);
+
+        assert_eq!(v.int_field(), 1);
+        v.set_int_field(3);
+        assert_eq!(v.int_field(), 3);
     }
 }
--- qt_ritual/test_assets/moqt/moqt_core/basic_class.h
+++ qt_ritual/test_assets/moqt/moqt_core/basic_class.h
@@ -13,7 +13,7 @@ public:
     void setFoo(int foo);
     int foo();
 
-    void overloaded_normal_const_and_static();
+    /*void overloaded_normal_const_and_static();
     void overloaded_normal_const_and_static() const;
     static void overloaded_normal_const_and_static(int);
 
@@ -31,10 +31,6 @@ public:
 
     float overloaded_args_and_return_type(int) { return 0; }
 
-    int public_int_field;
-    int *public_int_pointer_field;
-    int &public_int_reference_field;
-    BasicClassField public_class_field;
 
     BasicClass &overloaded_args_returns_ref(int) { return *this; }
 
@@ -44,7 +40,12 @@ public:
     void overloaded_exact_sized_args(uint16_t a) {}
 
     void overloaded_platform_dependent(int a) {}
-    void overloaded_platform_dependent(uint16_t b) {}
+    void overloaded_platform_dependent(uint16_t b) {} */
+
+    int int_field;
+    int *intPointerField;
+    int &intReference_field;
+    BasicClassField class_field;
 
 private:
     int m_foo;
