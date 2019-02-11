29998048621abcdbe3fbbc2baf6677b71423a09c
Pavel Strakhov <ri@idzaaus.org>
2019-02-12 00:20:59 +0300

    add moqt version test

---
+++ qt_ritual/crate_templates/moqt_core/tests/utils.rs
@@ -0,0 +1,21 @@
+use moqt_core::{moqt_abs, moqt_core_version};
+use std::ffi::CStr;
+
+#[test]
+fn abs() {
+    unsafe {
+        assert_eq!(moqt_abs(1), 1);
+        assert_eq!(moqt_abs(0), 0);
+        assert_eq!(moqt_abs(-2), 2);
+    }
+}
+
+#[test]
+fn version() {
+    unsafe {
+        // TODO: should return `CStr`
+        let r = moqt_core_version();
+        let str = CStr::from_ptr(r.as_ptr()).to_str().unwrap();
+        assert_eq!(str, "0.0.1");
+    }
+}
