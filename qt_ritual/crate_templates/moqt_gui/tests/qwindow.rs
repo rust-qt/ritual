6ed7af7e3624e7b64fc92b222c2ef860f362ff35
Pavel Strakhov <ri@idzaaus.org>
2019-02-16 00:02:26 +0300

    More QWindow tests

--- qt_ritual/crate_templates/moqt_gui/tests/qwindow.rs
+++ qt_ritual/crate_templates/moqt_gui/tests/qwindow.rs
@@ -1,14 +1,22 @@
-use moqt_gui::QWindow;
+use moqt_gui::{QWindow, get_window};
 use moqt_core::BasicClass;
 use cpp_utils::{CppBox, Ptr};
 
 #[test]
-fn create() {
+fn test_qwindow() {
     unsafe {
         let mut window = QWindow::new();
         let mut object: CppBox<BasicClass> = window.get_basic_class();
         assert_eq!(object.foo(), 42);
         let mut object_ptr: Ptr<BasicClass> = window.get_basic_class_ptr();
         assert_eq!(object_ptr.foo(), 43);
+    }
+}
+
+#[test]
+fn test_get_window() {
+    unsafe {
+        let window: Ptr<QWindow> = get_window();
+        assert!(window.is_null());
     }
 }
