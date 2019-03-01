d3be9451649a385b0c1db0228c0e89571de0cc8e
Pavel Strakhov <ri@idzaaus.org>
2019-03-01 21:56:17 +0300

    try CMAKE_INSTALL_RPATH_USE_LINK_PATH

--- scripts/ci/travis/run.bash
+++ scripts/ci/travis/run.bash
@@ -6,8 +6,6 @@ set -x
 export RUST_BACKTRACE=1
 export RITUAL_TEMP_TEST_DIR=$HOME/ritual_temp_test_dir
 
-export DYLD_LIBRARY_PATH=$DYLD_LIBRARY_PATH:/Users/travis/ritual_temp_test_dir/test_full_run/install/lib
-
 source "$TRAVIS_BUILD_DIR/scripts/ci/travis/setup_clang.bash"
 
 cd "$TRAVIS_BUILD_DIR"
