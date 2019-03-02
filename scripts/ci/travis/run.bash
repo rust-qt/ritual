f177c9767c495d4c9b465e851c8c92047d39322f
Pavel Strakhov <ri@idzaaus.org>
2019-03-02 19:23:40 +0300

    fix ci on macos

--- scripts/ci/travis/run.bash
+++ scripts/ci/travis/run.bash
@@ -6,13 +6,11 @@ set -x
 export RUST_BACKTRACE=1
 export RITUAL_TEMP_TEST_DIR=$HOME/ritual_temp_test_dir
 
-source "$TRAVIS_BUILD_DIR/scripts/ci/travis/setup_clang.bash"
+export DYLD_LIBRARY_PATH=$DYLD_LIBRARY_PATH:/Library/Developer/CommandLineTools/usr/lib
 
 cd "$TRAVIS_BUILD_DIR"
 rustup component add clippy
 rustup component add rustfmt
-
-echo $DYLD_LIBRARY_PATH
 
 cargo clippy --all-targets -- -D warnings
 cargo test -v
