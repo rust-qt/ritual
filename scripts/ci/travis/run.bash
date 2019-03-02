5ce3930997909f65137777d2e081d464b28194d6
Pavel Strakhov <ri@idzaaus.org>
2019-03-02 19:36:20 +0300

    fix ci clang for linux

--- scripts/ci/travis/run.bash
+++ scripts/ci/travis/run.bash
@@ -6,7 +6,7 @@ set -x
 export RUST_BACKTRACE=1
 export RITUAL_TEMP_TEST_DIR=$HOME/ritual_temp_test_dir
 
-export DYLD_LIBRARY_PATH=$DYLD_LIBRARY_PATH:/Library/Developer/CommandLineTools/usr/lib
+source "$TRAVIS_BUILD_DIR/scripts/ci/travis/setup_clang.bash"
 
 cd "$TRAVIS_BUILD_DIR"
 rustup component add clippy
--- scripts/ci/travis/setup_clang.bash
+++ scripts/ci/travis/setup_clang.bash
@@ -5,15 +5,7 @@
 set -o errexit
 
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
-    CLANG_DIR=$HOME/clang-3.8
-    cd "$HOME"
-    echo "Downloading libclang"
-    wget http://llvm.org/releases/3.8.0/clang+llvm-3.8.0-x86_64-apple-darwin.tar.xz -O - | tar -xJ
-    mv clang+llvm-3.8.0-x86_64-apple-darwin "$CLANG_DIR"
-
-    export LLVM_CONFIG_PATH=$CLANG_DIR/bin/llvm-config
-    export CLANG_SYSTEM_INCLUDE_PATH=$CLANG_DIR/lib/clang/3.8.0/include
-    export LD_LIBRARY_PATH=$CLANG_DIR/lib
+    export DYLD_LIBRARY_PATH=$DYLD_LIBRARY_PATH:/Library/Developer/CommandLineTools/usr/lib
 else
     export LLVM_CONFIG_PATH=/usr/lib/llvm-3.8/bin/llvm-config
     export CLANG_SYSTEM_INCLUDE_PATH=/usr/lib/llvm-3.8/lib/clang/3.8.0/include
