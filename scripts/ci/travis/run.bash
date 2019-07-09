f377bc5b35fc2cabc03c16a90a36f55a0f2ed80e
Pavel Strakhov <ri@idzaaus.org>
2019-07-09 23:15:22 +0300

    edit CI config

--- scripts/ci/travis/run.bash
+++ scripts/ci/travis/run.bash
@@ -6,12 +6,31 @@ set -x
 export RUST_BACKTRACE=1
 export RITUAL_TEMP_TEST_DIR=$HOME/ritual_temp_test_dir
 
-source "$TRAVIS_BUILD_DIR/scripts/ci/travis/setup_clang.bash"
+if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
+    export DYLD_LIBRARY_PATH=$DYLD_LIBRARY_PATH:/Library/Developer/CommandLineTools/usr/lib
+elif [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
+    sudo apt-get update
+    sudo apt-get install llvm-3.8 libclang-3.8-dev --yes --force-yes
+    export LLVM_CONFIG_PATH=/usr/lib/llvm-3.8/bin/llvm-config
+    export CLANG_SYSTEM_INCLUDE_PATH=/usr/lib/llvm-3.8/lib/clang/3.8.0/include
+elif [[ "$TRAVIS_OS_NAME" == "windows" ]]; then
+    curl -o "$TEMP/sqlite.zip" "https://www.sqlite.org/2016/sqlite-dll-win64-x64-3150100.zip"
+    export SQLITE3_LIB_DIR=$TEMP/sqlite
+    7z x "$TEMP/sqlite.zip" -o"$SQLITE3_LIB_DIR"
+    cmd.exe /C '"C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\VC\Auxiliary\Build\vcvarsall.bat" amd64 && lib /def:%SQLITE3_LIB_DIR%\sqlite3.def /out:%SQLITE3_LIB_DIR%\sqlite3.lib'
+    export PATH=$PATH:$SQLITE3_LIB_DIR
+fi
 
 cd "$TRAVIS_BUILD_DIR"
 rustup component add clippy
 rustup component add rustfmt
 
-cargo clippy --all-targets -- -D warnings
-cargo test -v
+# TODO: reverse, disable verbose
+
+if [[ "$TRAVIS_OS_NAME" == "windows" ]]; then
+cmd.exe /C '"C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\VC\Auxiliary\Build\vcvarsall.bat" amd64 && cargo test --color=always -v'
+else
+    cargo test --color=always -v
+fi
+cargo clippy --color=always --all-targets -- -D warnings
 cargo fmt -- --check
