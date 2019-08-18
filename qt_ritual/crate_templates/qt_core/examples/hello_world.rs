232eaf17ffed1eb1a97b29eec2998400883fb4b0
Pavel Strakhov <ri@idzaaus.org>
2019-08-18 15:24:33 +0300

    move out examples

--- qt_ritual/crate_templates/qt_core/examples/hello_world.rs
+++
@@ -1,8 +0,0 @@
-use qt_core::{q_debug, QCoreApplication, QString};
-
-fn main() {
-    QCoreApplication::init(|_app| unsafe {
-        let _ = q_debug!() << QString::from_std_str("Hello World!").as_ref();
-        0
-    })
-}
