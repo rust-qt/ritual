0b9c2fe87d7ae0b74f190a15f6fe2662e2303bc6
Pavel Strakhov <ri@idzaaus.org>
2019-10-27 19:52:24 +0300

    bump versions

--- qt_ritual/src/versions.rs
+++ qt_ritual/src/versions.rs
@@ -1,5 +1,5 @@
 /// Actual version of `qt_ritual_build` crate used as build dependency
-pub const QT_RITUAL_BUILD_VERSION: &str = "0.1.0";
+pub const QT_RITUAL_BUILD_VERSION: &str = "0.2.0";
 
 /// Version of all generated Qt crates
-pub const QT_OUTPUT_CRATES_VERSION: &str = "0.3.0";
+pub const QT_OUTPUT_CRATES_VERSION: &str = "0.4.0-alpha1";
