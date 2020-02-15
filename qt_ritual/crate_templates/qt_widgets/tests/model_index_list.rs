cfcfdd223d8209379d20cfc8584e9cbe695b57fa
Pavel Strakhov <ri@idzaaus.org>
2020-02-15 20:09:24 +0300

    update qt test

--- qt_ritual/crate_templates/qt_widgets/tests/model_index_list.rs
+++ qt_ritual/crate_templates/qt_widgets/tests/model_index_list.rs
@@ -1,10 +1,11 @@
+use cpp_core::NullPtr;
 use qt_widgets::{QApplication, QListView, SlotOfQListOfQModelIndex};
 
 #[test]
 fn model_index_list_slot() {
     QApplication::init(|_| unsafe {
         let list = QListView::new_0a();
-        let slot = SlotOfQListOfQModelIndex::new();
+        let slot = SlotOfQListOfQModelIndex::new(NullPtr, |_| {});
         let c = list.indexes_moved().connect(&slot);
         assert!(c.is_valid());
         0
