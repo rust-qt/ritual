9c9a2475935a78587634c330830670b1444ea9cd
Pavel Strakhov <ri@idzaaus.org>
2020-03-14 19:40:32 +0300

    fix warning in q_chart test

--- qt_ritual/crate_templates/qt_charts/tests/q_chart.rs
+++ qt_ritual/crate_templates/qt_charts/tests/q_chart.rs
@@ -3,7 +3,7 @@ use qt_charts::{qt_core::QString, qt_widgets::QApplication, QChart};
 #[test]
 fn chart1() {
     QApplication::init(|_| unsafe {
-        let mut chart = QChart::new_0a();
+        let chart = QChart::new_0a();
         chart.set_title(&QString::from_std_str("test"));
         let title = chart.title().to_std_string();
         assert_eq!(&title, "test");
